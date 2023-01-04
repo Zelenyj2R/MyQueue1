53fdcd1535915b81421335447e4001f4ebcd89cb
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-04 18:51:45 +0100

    Update MyQueueTest.java

--- src/MyQueueTest.java
+++ src/MyQueueTest.java
@@ -1,14 +1,12 @@
 public class MyQueueTest {
     public static void main(String[] args) {
-        MyQueue <String> myQueue = new MyQueue();
+        MyQueue<String> myQueue = new MyQueue();
         myQueue.add("Mercury");
         myQueue.add("Venus");
         myQueue.add("Earth");
         System.out.println("myQueue " + myQueue.size());
-        System.out.println("myQueue " + myQueue.peek(0));
+        System.out.println("myQueue " + myQueue.peek(1));
         System.out.println(myQueue.poll());
-
-
         myQueue.clear();
         System.out.println("myQueue " + myQueue.size());
         myQueue.add("Mercury");
